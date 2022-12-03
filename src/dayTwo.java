ad79b75baf191aefeeb392da492eede0f3465624
smuranjan1 <smyan.muranjan@gmail.com>
2022-12-02 20:39:13 -0500

    dayTwo Complete

--- src/dayTwo.java
+++ src/dayTwo.java
@@ -1,3 +1,131 @@
+import java.io.RandomAccessFile;
 public class dayTwo {
+    
+    public static void partOne(String filePath) throws Exception
+    {
+        RandomAccessFile file = new RandomAccessFile(filePath, "r");
+        String str;
+        int total = 0;
+        while ((str = file.readLine()) != null)
+        {
+            int firstScore = 0;
+            int secondScore = 0;
 
+            switch(str.charAt(2))
+            {
+                case 'X':
+                    firstScore = 1;
+                    switch(str.charAt(0))
+                    {
+                        case 'A':
+                            secondScore = 3;
+                            break;
+                        case 'B':
+                            secondScore = 0;
+                            break;
+                        case 'C':
+                            secondScore = 6;
+                            break;
+                    }
+                    break;
+                case 'Y':
+                    firstScore = 2;
+                    switch(str.charAt(0))
+                    {
+                        case 'A':
+                            secondScore = 6;
+                            break;
+                        case 'B':
+                            secondScore = 3;
+                            break;
+                        case 'C':
+                            secondScore = 0;
+                            break;
+                    }
+                    break;
+                case 'Z':
+                    firstScore = 3;
+                    switch(str.charAt(0))
+                    {
+                        case 'A':
+                            secondScore = 0;
+                            break;
+                        case 'B':
+                            secondScore = 6;
+                            break;
+                        case 'C':
+                            secondScore = 3;
+                            break;
+                    }
+                    break;
+            }
+
+            total += firstScore + secondScore;
+        }
+        System.out.println(total);
+    }
+
+    public static void partTwo(String filepath) throws Exception
+    {
+        RandomAccessFile file = new RandomAccessFile(filepath, "r");
+        String str;
+        int total = 0;
+        while ((str = file.readLine()) != null)
+        {
+            int firstScore = 0;
+            int secondScore = 0;
+
+            switch(str.charAt(2))
+            {
+                case 'X':
+                    firstScore = 0;
+                    switch(str.charAt(0))
+                    {
+                        case 'A':
+                            secondScore = 3;
+                            break;
+                        case 'B':
+                            secondScore = 1;
+                            break;
+                        case 'C':
+                            secondScore = 2;
+                            break;
+                    }
+                    break;
+                case 'Y':
+                    firstScore = 3;
+                    switch(str.charAt(0))
+                    {
+                        case 'A':
+                            secondScore = 1;
+                            break;
+                        case 'B':
+                            secondScore = 2;
+                            break;
+                        case 'C':
+                            secondScore = 3;
+                            break;
+                    }
+                    break;
+                case 'Z':
+                    firstScore = 6;
+                    switch(str.charAt(0))
+                    {
+                        case 'A':
+                            secondScore = 2;
+                            break;
+                        case 'B':
+                            secondScore = 3;
+                            break;
+                        case 'C':
+                            secondScore = 1;
+                            break;
+                    }
+                    break;
+            }
+
+            total += firstScore + secondScore;
+        }
+        System.out.println(total);
+    }
 }
--- src/tester.java
+++ src/tester.java
@@ -1,7 +1,9 @@
 public class tester {
     public static void main(String[] args) throws Exception 
     {
-        dayOne.partOne("C:\\Users\\smyan\\Documents\\github\\adventOfCode\\lib\\dayOne.txt");
-        dayOne.partTwo("C:\\Users\\smyan\\Documents\\github\\adventOfCode\\lib\\dayOne.txt");
+        // dayOne.partOne("C:\\Users\\smyan\\Documents\\github\\adventOfCode\\lib\\dayOne.txt");
+        // dayOne.partTwo("C:\\Users\\smyan\\Documents\\github\\adventOfCode\\lib\\dayOne.txt");
+        // dayTwo.partOne("C:\\Users\\smyan\\OneDrive\\Documents\\github\\adventOfCode\\lib\\dayTwo.txt");
+        // dayTwo.partTwo("C:\\Users\\smyan\\OneDrive\\Documents\\github\\adventOfCode\\lib\\dayTwo.txt");
     }
 }
